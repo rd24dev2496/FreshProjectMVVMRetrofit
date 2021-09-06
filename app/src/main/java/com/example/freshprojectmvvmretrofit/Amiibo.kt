b4ea9ec1857f0a38df42f8baf648ce1289a4b694
Rishabh <rd24dev2496>
2021-09-06 23:37:25 +0530

    Json response

--- app/src/main/java/com/example/freshprojectmvvmretrofit/Amiibo.kt
+++ app/src/main/java/com/example/freshprojectmvvmretrofit/Amiibo.kt
@@ -1,3 +1,18 @@
 package com.example.freshprojectmvvmretrofit
 
-data class Amiibo(val amiiboSeries: String, val character: String, val gameSeries: String, val head: String,val name : String,val imageUrl:String)
+class Amiibo {
+    var amiiboSeries: String? = null
+    var character: String? = null
+    var gameSeries: String? = null
+    var head: String? = null
+    var imageUrl: String? = null
+    var name: String? = null
+    //  var release: Release? = null
+    var tail: String? = null
+    var type: String? = null
+}
+
+class Root {
+    var amiibo: List<Amiibo>? = null
+}
+//data class Amiibo(val amiiboSeries: String, val character: String, val gameSeries: String, val head: String,val name : String,val imageUrl:String)
---
+++ app/src/main/java/com/example/freshprojectmvvmretrofit/JSONResponse.kt
@@ -0,0 +1,19 @@
+package com.example.freshprojectmvvmretrofit
+
+class JSONResponse {
+
+
+    class Amiibo {
+        var amiiboSeries: String? = null
+        var character: String? = null
+        var gameSeries: String? = null
+        var head: String? = null
+        var image: String? = null
+        var name: String? =null
+      //  var release: Release? = null
+        var tail: String? = null
+        var type: String? = null
+    }
+
+
+}
